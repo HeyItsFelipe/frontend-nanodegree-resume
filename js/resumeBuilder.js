05f6bfdd87de8b29f6c0867decd27d3104d7776e
HeyItsFelipe <heyitsfelipe@gmail.com>
2015-12-28 15:57:15 -0800

    Made changes to Project 2 Submittal after first review

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,7 +20,7 @@ var bio = {
 		"HTML", "CSS", "JavaScript", "Bootstrap", "JQuery"
 	],
 		"bioPic" : "images/me_300x300.jpg"
-}
+};
 
 var education = {
 	"schools" : [
@@ -30,7 +30,7 @@ var education = {
 			"degree" : "Bachelor's",
 			"majors" : ["Computer Engineering"],
 			"dates" : "Graduated: June 2009",
-			"url" : "http://www.csupomona.edu"
+			"url" : "http://www.cpp.edu"
 		}
 	],
 	"onlineCourses" : [
@@ -38,16 +38,16 @@ var education = {
 			"title" : "Introduction to Programming Nanodegree",
 			"school" : "Udacity",
 			"dates" : "Completed: October 2015",
-			"url" : "http://www.udacity.com/"
+			"url" : "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
 		},
 		{
-			"title" : "Front-End Web Design Nanodegree",
+			"title" : "Front-End Web Developer Nanodegree",
 			"school" : "Udacity",
 			"dates" : "In Progress",
-			"url" : "http://www.udacity.com/"
+			"url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
 		}
 	]
-}
+};
 
 var work = {
 	"jobs" : [
@@ -56,38 +56,43 @@ var work = {
 			"title" : "Teacher",
 			"location" : "Chino, CA",
 			"dates" : "August 2015 - Present",
-			"description" : "Teaching children mathematics, reading, and writing."
+			"description" : "Teaching children mathematics, reading, and writing.",
+			"url" : "http://locations.sylvanlearning.com/us/chino-ca/"
 		},
 		{
 			"employer" : "IES Engineering",
 			"title" : "Instrumentation and Controls Engineer",
 			"location" : "Bakersfield, CA",
 			"dates" : "May 2014 - February 2015",
-			"description" : "Delivered pizza."
+			"description" : "Worked with Control Narratives, Instrument Indexes, and I/O Lists.",
+			"url" : "http://www.ies-engr.com/"
 		},
 		{
 			"employer" : "WorleyParsons",
 			"title" : "Engineer I",
 			"location" : "Arcadia, CA",
 			"dates" : "September 2011 - August 2013",
-			"description" : "Delivered pizza."
+			"description" : "Worked with Instrument Index database.",
+			"url" : "http://www.worleyparsons.com/Pages/Default.aspx"
 		},
 		{
 			"employer" : "Multi-Cultural Dance Center",
 			"title" : "Dance Instructor",
 			"location" : "Hesperia, CA",
 			"dates" : "July 2011 - December 2011",
-			"description" : "Delivered pizza."
+			"description" : "Taught students beginner level salsa dancing.",
+			"url" : "http://www.mcdchesperia.com/"
 		},
 		{
 			"employer" : "Mobile Binaries",
 			"title" : "Telemarketer",
 			"location" : "Irvine, CA",
 			"dates" : "February 2011 - April 2011",
-			"description" : "Delivered pizza."
+			"description" : "Called potential client and educated them about our product.",
+			"url" : "http://www.mobilebinaries.com/"
 		}
 	]
-}
+};
 
 var projects = {
 	"projects" : [
@@ -97,10 +102,11 @@ var projects = {
 			"description" : "Notes of my Intro to Programming Nanodegree Udacity course.",
 			"images" : [
 				"images/project1_400x200.jpg"
-			]
+			],
+			"url" : "http://first-project-1029.appspot.com"
 		}
 	]
-}
+};
 
 bio.display = function() {
 
@@ -140,25 +146,26 @@ bio.display = function() {
 
 	/* Appending Skills */
 
-	if(bio["skills"].length > 0) {
+	if(bio.skills.length > 0) {
 
 		$("#header").append(HTMLskillsStart);
 
-		for (skill in bio.skills) {
+		for (var skill in bio.skills) {
 
 			var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
 			$("#skills").append(formattedSkill);
 		}
 	}
-}
+};
 
 work.display = function() {
 
-	for (job in work.jobs) {
+	for (var job in work.jobs) {
 
 		$("#workExperience").append(HTMLworkStart);
 
 		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		formattedEmployer = formattedEmployer.replace("#", work.jobs[job].url);
 		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 		var formattedEmployerTitle = formattedEmployer + formattedTitle;
 		$(".work-entry:last").append(formattedEmployerTitle);
@@ -170,16 +177,17 @@ work.display = function() {
 		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
 		$(".work-entry:last").append(formattedDescription);
 	}
-}
+};
 
 
 education.display = function() {
 
-	for (school in education.schools) {
+	for (var school in education.schools) {
 
 		$("#education").append(HTMLschoolStart);
 
 		var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		formattedSchoolName = formattedSchoolName.replace("#", education.schools[school].url);
 		var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
 		var formattedSchoolNameDegree = formattedSchoolName + formattedSchoolDegree;
 		$(".education-entry:last").append(formattedSchoolNameDegree);
@@ -197,11 +205,12 @@ education.display = function() {
 
 	$("#education").append(HTMLonlineClasses);
 
-	for (course in education.onlineCourses) {
+	for (var course in education.onlineCourses) {
 
 		$("#education").append(HTMLschoolStart);
 
 		var formattedCourseTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+		formattedCourseTitle = formattedCourseTitle.replace("#", education.onlineCourses[course].url);
 		var formattedCourseSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
 		var formattedCourseTitleSchool = formattedCourseTitle + formattedCourseSchool;
 		$(".education-entry:last").append(formattedCourseTitleSchool);
@@ -210,17 +219,19 @@ education.display = function() {
 		$(".education-entry:last").append(formattedCourseDates);
 
 		var formattedCourseURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+		formattedCourseURL = formattedCourseURL.replace("#", education.onlineCourses[course].url);
 		$(".education-entry:last").append(formattedCourseURL);
 	}
-}
+};
 
 projects.display = function() {
 
-	for (project in projects.projects) {
+	for (var project in projects.projects) {
 
 		$("#projects").append(HTMLprojectStart);
 
 		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		formattedTitle = formattedTitle.replace("#", projects.projects[project].url);
 		$(".project-entry:last").append(formattedTitle);
 
 		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
@@ -232,7 +243,7 @@ projects.display = function() {
 		var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
 		$(".project-entry:last").append(formattedImage);
 	}
-}
+};
 
 /* Display functions to display data */
 
